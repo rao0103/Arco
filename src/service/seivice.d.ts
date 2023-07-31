6af08c60ce068d42f1e6c13e9e90eafa7584e294
taFuture <3483988573@qq.com>
2023-07-31 09:00:44 +0000

    !1 工作台部分内容以及http.ts的内容更改 * feat: 工作台部分内容以及http.ts文件的更改

--- src/service/seivice.d.ts
+++ src/service/seivice.d.ts
@@ -8,3 +8,47 @@ type ILoginResponse = IBaseResponse<{
   csrf: string;
   refreshToken: string;
 }>;
+type ResponseData = {
+  id: number;
+  user_id_github: number;
+  username: string;
+  department_id: object;
+  nickname: string;
+  email: string;
+  state: number;
+  phone: string;
+  avatar: string;
+  company: string;
+  city: string;
+  last_login: string;
+  created_at: string;
+  updated_at: string;
+  roles: {
+    id: number;
+    name: string;
+    is_default: number;
+    created_at: string;
+    updated_at: string;
+    user_roles: {
+      id: number;
+      user_id: number;
+      role_id: number;
+      created_at: string;
+      updated_at: string;
+    };
+    permissions: {
+      id: number;
+      url: string;
+      action: string;
+      role_permissions: {
+        id: number;
+        role_id: number;
+        permission_id: number;
+        created_at: string;
+        updated_at: string;
+      };
+    }[];
+  }[];
+  permissions: string[];
+};
+type IuserInfo = IBaseResponse<ResponseData>
